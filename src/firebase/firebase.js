4636de80d1bb9cba193e78eb12f596f0a7b21165
MarcSa <marc@marcvirtual.com>
2021-10-08 18:44:26 +0200

    lesson 155

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -24,13 +24,13 @@ import firebase from 'firebase'
 // } from "firebase/database"
 
 const firebaseConfig = {
-	apiKey: "AIzaSyABif5vCeo6_BB047cTmam7wShiMsN8Nqk",
-	authDomain: "expensify-d3f79.firebaseapp.com",
-	databaseURL: "https://expensify-d3f79-default-rtdb.europe-west1.firebasedatabase.app",
-	projectId: "expensify-d3f79",
-	storageBucket: "expensify-d3f79.appspot.com",
-	messagingSenderId: "593844685122",
-	appId: "1:593844685122:web:807f8e314914dba17ca0db"
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.FIREBASE_DATABASE_URL,
+	projectId: process.env.FIREBASE_PROJECT_ID,
+	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.FIREBASE_APP_ID
 }
 
 firebase.initializeApp(firebaseConfig)
