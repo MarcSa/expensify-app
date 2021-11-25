4643c7254526fa5f32e12997acbd5d4fcbc0df10
MarcSa <marc@marcvirtual.com>
2021-11-25 11:49:21 +0100

    lets see

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -32,7 +32,7 @@ const firebaseConfig = {
 	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
 	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
 	appId: process.env.FIREBASE_APP_ID
-}
+} 
 
 firebase.initializeApp(firebaseConfig)
 
